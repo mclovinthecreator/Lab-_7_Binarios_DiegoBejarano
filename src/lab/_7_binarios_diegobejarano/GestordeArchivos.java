501c75abeee7597748cda11420e6a1ea69be2f75
Diego Bejarano <diegoalejandrop78@gmail.com>
2026-03-13 09:55:41 -0600

    Termine la clase que gestiona los archivos

--- src/lab/_7_binarios_diegobejarano/GestordeArchivos.java
+++ src/lab/_7_binarios_diegobejarano/GestordeArchivos.java
@@ -17,8 +17,44 @@ public class GestordeArchivos {
             oos.writeObject(oos);
             oos.close();
             fos.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Error al guardar: "+ e.getMessage());
+        }
+    }
+    
+    public static ArrayList<Cancion> cargarlista(){
+        File archivo = new File(archivolista);
+        if(!archivo.exists()){
+            return new ArrayList();
+        }
+        try {
+            FileInputStream fis = new FileInputStream(archivolista);
+             ObjectInputStream ois = new ObjectInputStream(fis);
+             ArrayList<Cancion> lista =  (ArrayList<Cancion>) ois.readObject();
+             ois.close();
+             fis.close();
+             return lista;
+            
+        } catch (IOException e) {
+            System.out.println("Error IO: "+ e.getMessage());
+            return new ArrayList();
+        }catch(ClassNotFoundException e){
+            System.out.println("Error clase: "+ e.getMessage());
+            return new ArrayList();
+        }
+    }
+    
+    public static byte[] leerimagen(File archivo){
+        try {
+            FileInputStream fis = new FileInputStream(archivo);
+            byte[] bytes = new byte[(int) archivo.length()];
+            fis.read(bytes);
+            fis.close();
+            return bytes;
+            
+        } catch (IOException e) {
+            System.out.println("Error leyendo imagen: "+ e.getMessage());
+            return null;
         }
     }
 }
