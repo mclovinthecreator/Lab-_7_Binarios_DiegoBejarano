5ea889ee96355c54326c1626e8fd760e32c7b362
Diego Bejarano <diegoalejandrop78@gmail.com>
2026-03-13 08:29:46 -0600

    commit de prueba

---
+++ src/lab/_7_binarios_diegobejarano/Lab_7_Binarios_DiegoBejarano.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab._7_binarios_diegobejarano;
+
+/**
+ *
+ * @author diego
+ */
+public class Lab_7_Binarios_DiegoBejarano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
